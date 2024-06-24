70bd07f020106ee1b4c8ecf06a1a27cde15c6575
Felipegithubf <felipecarvalho5055@gmail.com>
2024-06-23 23:18:00 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+export const items = [
+  {date: new Date(2024 , 9 , 15) , category: 'food' , title:'Mc Donalds' , value: 50.12},
+  {date: new Date(2024 , 9 , 15) , category: 'food' , title:'Burguer King ' , value: 28},
+  {date: new Date(2024 , 9 , 15) , category: 'rent' , title:'Aluguel Aprt' , value: 2300},
+  {date: new Date(2024 , 9 , 15) , category: 'salry' , title:'Salário' , value: 2300},
+
+
+]
